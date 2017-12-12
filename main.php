865131d824ad41b80ba1aa91569feb61fd77ae41
Aaron Lin <aaronl@berkeley.edu>
2017-12-11 23:12:04 -0800

    e

--- main.php
+++ main.php
@@ -1,12 +1,2 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Aaron Lin</title>
-</head>
-<body>
-<h1>HI LOL</h1>
-
-<p class='green'>This is a test page hehehehehe</p>
-
-</body>
-</html>
+# Hello World
+## My name is Aaron Lin
