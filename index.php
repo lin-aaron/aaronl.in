aaed131c8e0cc5fd9f05ac02104a19d5b43067fb
Aaron Lin <aaronl@berkeley.edu>
2018-09-09 19:09:51 -0700

    markdown to basic html

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <link href="styles.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>You've reached Aaron Lin's website.</h1>
+    <p>I'm a third-year undergrad at UC Berkeley, studying Electrical Engineering and Computer Science.
+      I'm on <a href="https://github.com/lin-aaron/">Github</a> and <a href="https://linkedin.com/in/aaron-lin8">LinkedIn</a>.
+      Here is my <a href="resources/cv.pdf">resume</a>. I'm currently looking for internship opportunities in software engineering for the summer for 2019!
+      Email me at aaron (dot) lin [at] berkeley {dot} edu.</p>
+  </body>
+</html>
